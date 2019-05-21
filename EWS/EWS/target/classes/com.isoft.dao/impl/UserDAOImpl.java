34a785b68b75c6273ae28a08e34a1550c216c9cb
aerosix <1005925523@qq.com>
2019-05-21 18:49:55 +0800

    5-21

---
+++ EWS/EWS/target/classes/com.isoft.dao/impl/UserDAOImpl.java
@@ -0,0 +1,39 @@
+package com.isoft.dao.impl;
+
+import com.isoft.dao.IUserDAO;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository("userDAO")
+public class UserDAOImpl implements IUserDAO {
+    @Autowired
+    SqlSessionFactoryBean sessionFactoryBean;
+    @Override
+    public String login(String uname, String upwd) {
+        SqlSession sqlSession= sessionFactoryBean.openSession( b: ture);
+        String statment="com.isoft.userMapper.login";
+        Map map=new HashMap();
+        map.put("uname",uname);
+        map.put("upwd",upwd);
+        List<Map<String,Object>> list = sqlSession.selectList(statment,);
+        if (list.size()>0)
+            return "success";
+        else
+            return "fault";
+      /*  System.out.println(uname+","+upwd);
+        return "success";*/
+    }
+
+    @Override
+    public boolean register(String uname, String upwd, String email) {
+        System.out.println(email);
+        return true;
+    }
+}
